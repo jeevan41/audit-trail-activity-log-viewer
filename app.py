308ae9f5a3a2fffcbd634d9a5a05312caa382242
jeevan41 <jeeang12005@gmail.com>
2026-05-10 21:35:58 +0530

    Day-8 is done

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ import time
 app = Flask(__name__)
 CORS(app)
 
+@app.after_request
+def add_security_headers(response):
+
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    response.headers["X-Frame-Options"] = "DENY"
+    response.headers["X-XSS-Protection"] = "1; mode=block"
+    response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
+    response.headers["Content-Security-Policy"] = "default-src 'self'"
+
+    return response
+
 app.register_blueprint(describe_bp)
 
 START_TIME = time.time()
