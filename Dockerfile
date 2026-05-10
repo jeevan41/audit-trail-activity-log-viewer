9b414aa0c4efc4bb2d84e9acf7eab4cf61cb6768
jeevan41 <jeeang12005@gmail.com>
2026-05-10 23:47:09 +0530

    Day-13 done

--- Dockerfile
+++ Dockerfile
@@ -11,3 +11,18 @@ COPY . .
 EXPOSE 5000
 
 CMD ["python", "app.py"]
+
+
+FROM python:3.11
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
+COPY . .
+
+EXPOSE 5000
+
+CMD ["python", "app.py"]
---
+++ demo_records.py
@@ -0,0 +1,72 @@
+demo_records = [
+
+    {
+        "username": "admin",
+        "action_type": "LOGIN",
+        "module": "Authentication",
+        "status": "SUCCESS"
+    },
+
+    {
+        "username": "john",
+        "action_type": "DELETE",
+        "module": "User Management",
+        "status": "FAILED"
+    },
+
+    {
+        "username": "security_team",
+        "action_type": "MULTIPLE LOGIN ATTEMPTS",
+        "module": "Authentication",
+        "status": "FAILED"
+    },
+
+    {
+        "username": "manager",
+        "action_type": "EXPORT",
+        "module": "Reports",
+        "status": "SUCCESS"
+    },
+
+    {
+        "username": "employee1",
+        "action_type": "UPDATE",
+        "module": "Payroll",
+        "status": "SUCCESS"
+    },
+
+    {
+        "username": "auditor",
+        "action_type": "VIEW",
+        "module": "Audit Logs",
+        "status": "SUCCESS"
+    },
+
+    {
+        "username": "guest",
+        "action_type": "LOGIN",
+        "module": "Authentication",
+        "status": "FAILED"
+    },
+
+    {
+        "username": "developer",
+        "action_type": "CONFIG_CHANGE",
+        "module": "System Settings",
+        "status": "SUCCESS"
+    },
+
+    {
+        "username": "analyst",
+        "action_type": "DOWNLOAD",
+        "module": "Reports",
+        "status": "SUCCESS"
+    },
+
+    {
+        "username": "unknown_user",
+        "action_type": "MULTIPLE LOGIN ATTEMPTS",
+        "module": "Authentication",
+        "status": "FAILED"
+    }
+]
